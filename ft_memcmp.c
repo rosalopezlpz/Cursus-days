dc5ba1c7f18211c253b20736d30498bd85ed9b26
Rosa López <61782352+rosalopezlpz@users.noreply.github.com>
2022-10-24 22:17:58 +0200

    Updated version after 4 function and updating the Makefile
    Hemos incluido el strlcpy, memcpy, memchr, memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlopez-l <rlopez-l@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 21:23:43 by rlopez-l          #+#    #+#             */
+/*   Updated: 2022/10/24 21:46:38 by rlopez-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*int main(void)
+{
+		char *s = "barcelona";
+		char *p = "barcelona";
+		printf("%d",ft_memcmp(s, p, 6));
+		return (0);
+}*/
