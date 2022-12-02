07d4007f40a7412f98a8eebce5fcb9abdf2cc57e
Rosa López <61782352+rosalopezlpz@users.noreply.github.com>
2022-12-02 18:49:01 +0100

    Add files via upload

---
+++ Libft/ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlopez-l <rlopez-l@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 19:10:18 by rlopez-l          #+#    #+#             */
+/*   Updated: 2022/10/22 13:30:22 by rlopez-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{	
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((unsigned char *)b)[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+/*int	main(void)
+{
+	char b[]= "unatardedemayo";
+    int c = 'h';
+	size_t len = 3;
+
+	printf("%s\n", ft_memset(b, c, len));
+	return (0);
+}*/
